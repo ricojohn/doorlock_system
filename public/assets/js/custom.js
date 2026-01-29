ceff2351994fa77522657262aaeed385d7c64fbf
ricojohn <53473361+ricojohn@users.noreply.github.com>
2026-01-29 10:46:17 +0800

    Update custom.js

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // For DataTables
     // Reusable function to initialize DataTable
     function initDataTable(selector, emptyMessage) {
@@ -32,16 +32,51 @@ $(document).ready(function() {
         table.buttons().container().appendTo('#exportButtons');
     }
 
+    function initDataTableWithFilter(selector, emptyMessage) {
+        if ($(selector).length === 0) return;
+
+        const config = {
+            dom: 'lfrtip',
+            language: {
+                emptyTable: emptyMessage,
+                zeroRecords: emptyMessage,
+                searchPlaceholder: 'Search'
+            },
+            pageLength: 10,
+            responsive: true,
+            buttons: [
+                {
+                    extend: 'collection',
+                    text: '<i class="bi bi-file-earmark-excel"></i> Export',
+                    className: 'btn btn-success btn-sm',
+                    buttons: [
+                        { extend: 'copy', className: 'dropdown-item' },
+                        { extend: 'csv', className: 'dropdown-item' },
+                        { extend: 'excel', className: 'dropdown-item' },
+                        { extend: 'pdf', className: 'dropdown-item' },
+                    ]
+                }
+            ],
+            order: [
+                [0, 'asc'],
+            ],
+
+        };
+
+        const table = new DataTable(selector, config);
+        table.buttons().container().appendTo('#exportButtons');
+    }
+
     // Initialize all tables
     initDataTable('#members-table', 'No Member Available');
     initDataTable('#subscriptions-table', 'No Subscription Available');
     initDataTable('#plans-table', 'No Plan Available');
     initDataTable('#coaches-table', 'No Coach Available');
-    initDataTable('#access-logs-table', 'No Access Log Available');
+    initDataTableWithFilter('#access-logs-table', 'No Access Log Available');
 
     // Phone number format PH
     const contactInput = document.getElementById('phone');
-    contactInput.addEventListener('input', function(e) {
+    contactInput.addEventListener('input', function (e) {
         let digits = e.target.value.replace(/\D/g, ''); // only digits
 
         // Remove country code if user typed it manually
